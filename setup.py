0299375ff50d5ba53097ce5f4ea48ee5e3cf97a0
Anand A Joshi <ajoshi@sipi.usc.edu>
2016-08-20 23:06:45 -0700

    initial 0.0 version
    This is 0.0 version of sphere-kit. more coming soon.

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 20 22:50:32 2016
+
+@author: ajoshi
+"""
+
+from distutils.core import setup
+setup(
+  name = 'sphere-kit',
+  packages = ['sphere-kit'], # this must be the same as the name above
+  version = '0.0',
+  description = 'Computational tools for processing and analysis on hypersphere',
+  author = 'Anand A. Joshi',
+  author_email = 'ajoshi.usc@outlook.com',
+  url = 'https://github.com/ajoshiusc/sphere-kit', # use the URL to the github repo
+  download_url = 'https://github.com/ajoshiusc/sphere-kit/tarball/0.1', # I'll explain this in a second
+  keywords = ['sphere', 'signal processing', 'clustering', 'image analysis'], # arbitrary keywords
+  classifiers = [],
+)
---
+++ sphere-kit/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 20 22:38:33 2016
+
+@author: ajoshi
+"""
+
